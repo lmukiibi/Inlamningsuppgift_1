ad3547cf08234e2832718a480390913eb773f173
Leroy <leroy.mukiibi@gmail.com>
2020-11-18 01:04:47 +0100

    Add project files.

---
+++ v47_Inlämningsuppgift 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace v47_Inlämningsuppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opOne, opTwo, termOne, termTwo, termThree, result;
+            List<string> listOfResults = new List<string>()
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
